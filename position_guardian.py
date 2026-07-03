acc4129bbe94d2912c92a8603e126a8eccde2c5f
ricky-sejong <hgiwoo62@gmail.com>
2026-07-03 12:22:20 +0900

    Add User-Agent header to request

--- position_guardian.py
+++ position_guardian.py
@@ -123,6 +123,7 @@ class OKXClient:
             "OK-ACCESS-TIMESTAMP": ts,
             "OK-ACCESS-PASSPHRASE": self.pp,
             "Content-Type": "application/json",
+            "User-Agent":           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }
 
         url = self.BASE + full_path
